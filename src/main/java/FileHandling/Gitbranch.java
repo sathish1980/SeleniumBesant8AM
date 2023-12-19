f1cb138e4b6fc179babd050fdca7b645287e547d
sathish1980 <kumar.sathish189@gmail.com>
2023-12-19 08:56:47 +0530

    new

--- src/main/java/FileHandling/Gitbranch.java
+++ src/main/java/FileHandling/Gitbranch.java
@@ -2,14 +2,20 @@ package FileHandling;
 
 public class Gitbranch {
 
-public void add()
-{
-System.out.println("this is studentclass");
-}
+	public void add()
+	{
+		System.out.println("this is studentclass");
+	}
+
+	public void div()
+	{
+		System.out.println("this is div studentclass");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Gitbranch g = new Gitbranch();
+		g.div();
 	}
 
 }
