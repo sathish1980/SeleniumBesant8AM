df5f27ad6af330d73bf0cde231fb3f1054f2c45e
sathish1980 <kumar.sathish189@gmail.com>
2023-12-19 08:41:46 +0530

    git branch student

--- src/main/java/FileHandling/Gitbranch.java
+++ src/main/java/FileHandling/Gitbranch.java
@@ -2,6 +2,11 @@ package FileHandling;
 
 public class Gitbranch {
 
+public void add()
+{
+System.out.println("this is studentclass");
+}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
