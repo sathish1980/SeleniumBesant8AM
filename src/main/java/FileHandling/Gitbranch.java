2736f04727b358cc4ca2258a688c0b78cff333bc
sathish1980 <kumar.sathish189@gmail.com>
2023-12-19 08:42:59 +0530

    parent class

--- src/main/java/FileHandling/Gitbranch.java
+++ src/main/java/FileHandling/Gitbranch.java
@@ -2,9 +2,15 @@ package FileHandling;
 
 public class Gitbranch {
 
+	public void sub()
+	{
+		System.out.println("This is parent class");
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Gitbranch g = new Gitbranch();
+		g.sub();
 	}
 
 }
